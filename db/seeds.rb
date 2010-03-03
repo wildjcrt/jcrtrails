9be4c5337f71d8c3cbd05747615f1ca6534f5699
xdite <xdite@pixnet.tw>
2010-03-03 13:55:32 +0800

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+user = User.new(:login => "jcrt", :password => "123456", :password_confirmation => "123456", :email => "jcrt@pixnet.tw")
+user.save!
+
+user = User.new(:login => "xdite", :password => "123456", :password_confirmation => "123456", :email => "xdite@pixnet.tw")
+user.save!
