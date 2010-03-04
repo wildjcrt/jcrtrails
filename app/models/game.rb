ef8d651e29f335927cccdc0c407453085bf3359b
xdite <xdite@pixnet.tw>
2010-03-04 16:37:33 +0800

    game

--- app/models/game.rb
+++ app/models/game.rb
@@ -15,8 +15,10 @@
 #
 
 class Game < ActiveRecord::Base
-  has_many  :favorits
+  
+  has_many  :favorites
   has_many  :users, :through => :favorites
+  
   has_one   :album
   named_scope :recent  
 end
